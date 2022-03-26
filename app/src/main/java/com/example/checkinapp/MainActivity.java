ac2f1d7026556a31bbbbfe49a2f74151ba19948e
chrisholmes1128 <36671471+chrisholmes1128@users.noreply.github.com>
2022-03-26 11:14:38 -0400

    Added home page. Added login field verification and now will redirect user when correct email pass combination is entered.

--- app/src/main/java/com/example/checkinapp/MainActivity.java
+++ app/src/main/java/com/example/checkinapp/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.checkinapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView register;
+    private EditText editTextEmail, editTextPassword;
+    private Button login;
+
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +34,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         register = (TextView) findViewById(R.id.registerTextLink);
         register.setOnClickListener(this);
+
+        login = (Button) findViewById(R.id.loginBtn);
+        login.setOnClickListener(this);
+
+        editTextEmail = (EditText) findViewById(R.id.emailField);
+        editTextPassword = (EditText) findViewById(R.id.passwordField);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -26,6 +52,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.registerTextLink:
                 startActivity(new Intent(this, RegisterUser.class));
                 break;
+            case R.id.loginBtn:
+                loginUser();
+                break;
         }
+    }
+
+    private void loginUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String pass = editTextPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please enter a valid email address");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (pass.isEmpty()) {
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (pass.length() < 6) {
+            editTextPassword.setError("Minimum password length is 6 characters");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    startActivity(new Intent(MainActivity.this, HomePageActivity.class));
+                    progressBar.setVisibility(View.GONE);
+                } else {
+                    progressBar.setVisibility(View.GONE);
+                    Toast.makeText(MainActivity.this, "Failed to login, Incorrect email or password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
